7e226df49736389823e31e291ad06b07106efe73
Indy Iri <indy.iri@student.pxl.be>
2024-10-17 20:35:33 +0200

    Afgewerkt

--- H3Oef13Interest/H3Oef13Interest/MainWindow.xaml.cs
+++ H3Oef13Interest/H3Oef13Interest/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace H3Oef13Interest
         {
             initialCapitalTextBox.Clear();
             desiredFinalCapitalTextBox.Clear();
-            initialCapitalTextBox.Clear();
+            interestTextBox.Clear();
             resultTextBox.Clear();
         }
 
@@ -34,9 +34,40 @@ namespace H3Oef13Interest
             Close();
         }
 
+        StringBuilder sb = new StringBuilder();
+
         private void calculateButton_Click(object sender, RoutedEventArgs e)
         {
+            double initialCapital = 0.00;
+            double desiredFinalCapital = 0.00;
+            double interest = 0.00;
+            int amountOfYears = 0;
+            
+            string inputInitialCapital = initialCapitalTextBox.Text;
+            string inputDesiredFinalCapital = desiredFinalCapitalTextBox.Text;
+            string inputInterest = interestTextBox.Text;
 
+            bool isinputInitialCapitalValid = double.TryParse(inputInitialCapital, out initialCapital);
+            bool isinputDesiredFinalCapitalValid = double.TryParse(inputDesiredFinalCapital, out desiredFinalCapital);
+            bool isinputInterestValid = double.TryParse(inputInterest, out interest);
+
+            if (isinputInitialCapitalValid && initialCapital > 0 && desiredFinalCapital > 0 && isinputDesiredFinalCapitalValid && isinputInterestValid && interest > 0 )
+            {
+                while (initialCapital < desiredFinalCapital)
+                {
+                    initialCapital *= (1 + (interest / 100));
+                    amountOfYears++;
+
+                    sb.AppendLine($"Waarde na {amountOfYears} jaren: €{Math.Round(initialCapital, 2)}");
+                    
+                    //resultTextBox.Text = $"Waarde na {amountOfYears} jaren: €{Math.Round(initialCapital, 2)}";
+                }
+                resultTextBox.Text = sb.ToString();
+            }
+            else if (!isinputInitialCapitalValid || !isinputDesiredFinalCapitalValid || !isinputInterestValid || interest <= 0) 
+            {
+                resultTextBox.Text = "Geef correcte waarden in: interest, begin- en eindkapitaal moeten getallen zijn groter dan 0";
+            }
         }
     }
 }
